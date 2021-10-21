5bb0f27e3f7be60e5b7f65522d63522ece86f38c
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-21 12:27:05 -0500

    Created IUserProfileRepository and UserProfileRepository classes

---
+++ MarvelAlliance/Repositories/UserProfileRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using MarvelAlliance.Models;
+using MarvelAlliance.Utils;
+
+namespace MarvelAlliance.Repositories
+{
+    public class UserProfileRepository : BaseRepository, IUserProfileRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+        public UserProfile GetByFirebaseUserId(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, FirstName, LastName, UserName, Email, FirebaseUserId, DateCreated
+                        FROM UserProfile
+                        WHERE FirebaseUserId = @firebaseuserId";
+
+                    DbUtils.AddParameter(cmd, "@firebaseUserId", firebaseUserId);
+
+                    UserProfile userProfile = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        userProfile = new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirstName = DbUtils.GetString(reader, "FirstName"),
+                            LastName = DbUtils.GetString(reader, "LastName"),
+                            UserName = DbUtils.GetString(reader, "UserName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated")
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+
+
+    }
+}
