50a2ee245efefa215f15e466a4556cce75cdd180
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-21 13:31:05 -0500

    Created UserProfileController class and methods to retrieve a UserProfile by firebase id, verify user exists, and register a new user to set up for leveraging Firebase auth

---
+++ MarvelAlliance/Controllers/UserProfileController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarvelAlliance.Repositories;
+using MarvelAlliance.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MarvelAlliance.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        //https://localhost:5001/api/userprofile/id
+        [HttpGet("{firebaseUserId}")]
+        public IActionResult GetByFirebaseUserId(string firebaseUserId)
+        {
+            var userProfile = _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        //https://localhost:5001/api/userprofile/DoesUserExist/id
+        [HttpGet("DoesUserExist/{firebaseUserId}")]
+        public IActionResult DoesUserExist(string firebaseUserId)
+        {
+            var userProfile = _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        //https://localhost:5001/api/userprofile/register
+        [HttpPost("register")]
+        public IActionResult Register(UserProfile userProfile)
+        {
+            try
+            {
+                userProfile.DateCreated = DateTime.Now;
+                _userProfileRepository.Add(userProfile);
+                return CreatedAtAction(
+                    nameof(GetByFirebaseUserId), new { firebaseUserId = userProfile.FirebaseUserId }, userProfile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500);
+            }
+        }
+
+        // https://localhost:5001/api/userprofile/
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            try
+            {
+                userProfile.DateCreated = DateTime.Now;
+                _userProfileRepository.Add(userProfile);
+                return CreatedAtAction("Get", new { id = userProfile.Id }, userProfile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500);
+            }
+        }
+    }
+}
