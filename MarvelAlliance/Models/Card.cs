cd473cd04d0ceb2284574992b126bb2deacecea9
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-21 10:33:37 -0500

    created Model classes for each entity in db

---
+++ MarvelAlliance/Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MarvelAlliance.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int DeckId { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Character Name...")]
+        [DisplayName("Character Name")]
+        public string CharacterName { get; set; }
+
+        [Required]
+        public int Health { get; set; }
+
+        [Required]
+        public int Power { get; set; }
+
+        [Required]
+        public int Speed { get; set; }
+
+        [Required]
+        public int Strength { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ MarvelAlliance/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarvelAlliance.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        [Required]
+        public int DeckId { get; set; }
+
+        [Required]
+        public string IsWon { get; set; }
+
+        public DateTime CompletionDate { get; set; }
+    }
+}
