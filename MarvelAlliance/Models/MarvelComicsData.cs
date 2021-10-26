bafa24622dbda4974b1abefef72d073265825d84
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-26 12:12:48 -0500

    Created model classes that take the shape of the JSON response data fetched from the Marvel Comics API

---
+++ MarvelAlliance/Models/MarvelComicsData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAlliance.Models
+{
+    public class MarvelComicsData
+    {
+        public List<MarvelComicsResult> Results { get; set; }
+    }
+}
---
+++ MarvelAlliance/Models/MarvelComicsResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAlliance.Models
+{
+    public class MarveComicsResponse
+    {
+        public MarvelComicsData Data { get; set; }
+    }
+
+   
+
+ 
+
+    
+
+ 
+
+  
+
+   
+
+   
+
+  
+
+   
+
+   
+
+}
