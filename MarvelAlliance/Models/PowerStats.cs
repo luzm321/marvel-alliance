cf86dbc8b7fd4cd8a82b8b8d021c43191ba6244d
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-10-26 12:00:19 -0500

    Created model classes that take the shape of the JSON response data fetched from the SuperHeroSearch API

---
+++ MarvelAlliance/Models/PowerStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAlliance.Models
+{
+    public class PowerStats
+    {
+        //public int intelligence { get; set; }
+        public int Strength { get; set; }
+        public int Speed { get; set; }
+        //public int durability { get; set; }
+        public int Power { get; set; }
+        //public int combat { get; set; }
+    }
+}
+
+
---
+++ MarvelAlliance/Models/SuperHeroSearchResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAlliance.Models
+{
+    public class SuperHeroSearchResponse
+    {
+        public string Name { get; set; }
+        public List<PowerStats> Powerstats { get; set; }
+        public List<Images> Images { get; set; }
+    }
+}
